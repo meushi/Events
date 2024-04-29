935efd361994fa97c1cbaa583ca7771bab97e769
meushi <taku73baseball@gmail.com>
2024-04-29 20:58:30 +0900

    CSSの編集

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,38 +1,33 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>トップページ</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <h1>投稿一覧</h1>
-        <a href='/posts/create/'>新規作成</a>
-            <h2 class='body'>
-                <a href="/posts/"></a>
-            </h2>
+<x-app-layout>
+    <div class="bg-gray-900 text-white">
+        <body>
+            <h1 class="flex items-center justify-center font-bold text-lg">投稿一覧</h1>
+            <a class="flex items-center justify-center font-bold" href='/posts/create/'>新規作成</a>
+                <h2 class='body'>
+                    <a href="/posts/"></a>
+                </h2>
+                <div>
+                    @foreach ($posts as $post)
+                        <div style='border:solid 1px; margin-bottom: 10px;'>
+                            <a href={{'/users/'.$post->user->id}}>{{$post->user->name}}</a>
+                            <a class='eventname'href={{'/events/'.$post->event->id}}>{{$post->event->name}}</a>
+                            <a class='venuename'href={{'/venues/'.$post->venue->id}}>{{$post->venue->name}}</a>
+                            <a class='performername'href={{'/performers/'.$post->performer->id}}>{{$post->performer->name}}</a>
+                            <div class="card-body">
+                                <p>{{Str::limit($post->body,100,'・・・')}}</p>
+                            </div>
+                            <div class="card-footer d-flex flex-wrap justify-content-between align-items-center px-0 pt-0 pb-3">
+                                <div class="px-4 pt-3"> 
+                                   <button type="button" class="btn btn-primary">
+                                      <a class="text-decoration-none" href='/posts/{{ $post->id }}' style="color:white;">詳細・コメントする</a>
+                                  </button> </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             <div>
-                @foreach ($posts as $post)
-                    <div style='border:solid 1px; margin-bottom: 10px;'>
-                        <a href={{'/users/'.$post->user->id}}>{{$post->user->name}}</a>
-                        <a class='eventname'href={{'/events/'.$post->event->id}}>{{$post->event->name}}</a>
-                        <a class='venuename'href={{'/venues/'.$post->venue->id}}>{{$post->venue->name}}</a>
-                        <a class='performername'href={{'/performers/'.$post->performer->id}}>{{$post->performer->name}}</a>
-                        <div class="card-body">
-                            <p>{{Str::limit($post->body,100,'・・・')}}</p>
-                        </div>
-                        <div class="card-footer d-flex flex-wrap justify-content-between align-items-center px-0 pt-0 pb-3">
-                            <div class="px-4 pt-3"> 
-                               <button type="button" class="btn btn-primary">
-                                  <a class="text-decoration-none" href='/posts/{{ $post->id }}' style="color:black;">詳細・コメントする</a>
-                              </button> </div>
-                        </div>
-                    </div>
-                @endforeach
+                <a href="/">トップページへ</a>
             </div>
-        <div>
-            <a href="/">トップページへ</a>
-        </div>
-    </body>
-</html>
+        </body>
+    </div>
+</x-app-layout>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,22 +1,24 @@
-<x-app-layout>
-        <h1 class="text-red-200">投稿詳細</h1>
+<x-app-layout >
+    <div class="bg-gray-900 text-white pb-20">
+        <h1 class="flex items-center justify-center font-bold text-lg">投稿詳細</h1>
         <div>
-            <p>投稿者：<a href={{'/users/'.$post->user->id}}>{{$post->user->name}}</a></p>
+            <p>投稿者：<a  href={{'/users/'.$post->user->id}}>{{$post->user->name}} </a></p>
             <p>イベント：<a href="/events/{{ $post->event->id }}">{{ $post->event->name }}</a></p>
             <p>本文：{{ $post->body }}</p>
-            <p>会場:<a href="/venues/{{ $post->venue->id }}">{{ $post->venue->name }}</a></p>
+            <p>会場:<a  href="/venues/{{ $post->venue->id }}">{{ $post->venue->name }}</a></p>
             <p>出演者:<a href="/performers/{{ $post->performer->id }}">{{ $post->performer->name }}</a></p>
         </div>
         <span>
-            <img src="{{asset('img/nicebutton.png')}}" width="30px">
              
             <!-- もし$niceがあれば＝ユーザーが「いいね」をしていたら -->
             @if($nice)
             <!-- 「いいね」取消用ボタンを表示 -->
             	<a href="{{ route('unnice', $post) }}" class="btn btn-success btn-sm">
             		いいね
+            		❤
+            		{{--<img src="{{asset('img/nicebutton.png')}}" width="30px">--}}
             		<!-- 「いいね」の数を表示 -->
-            		<span class="badge">
+            		<span class="text-red-500" class="badge">
             			{{ $post->post_likes->count() }}
             		</span>
             	</a>
@@ -24,6 +26,8 @@
             <!-- まだユーザーが「いいね」をしていなければ、「いいね」ボタンを表示 -->
             	<a href="{{ route('nice', $post) }}" class="btn btn-secondary btn-sm">
             		いいね
+            		🤍
+            		{{--<img src="{{asset('img/nicebutton.png')}}" width="30px">--}}
             		<!-- 「いいね」の数を表示 -->
             		<span class="badge">
             			{{ $post->post_likes->count() }}
@@ -32,15 +36,15 @@
             @endif
         </span>
         <div>
-            <p class="edit">[<a href="/posts/{{ $post->id }}/edit">編集</a>]</p>
+            <p>[<a href="/posts/{{ $post->id }}/edit">編集</a>]</p>
             <a href="/">戻る</a>
         </div>
-        <div class="card mb-4">
+        <div class="card mb-4 flex items-center justify-center">
             <form method="post" action="{{route('comment.store')}}">
                 @csrf
                 <input type="hidden" name='post_id' value={{$post->id}}>
                 <div class="form-group">
-                    <textarea name="body" class="form-control" id="body" cols="30" rows="5" 
+                    <textarea class="text-black"name="body" class="form-control" id="body" cols="30" rows="5" 
                     placeholder="コメントを入力する"></textarea>
                 </div>
                 <div class="form-group mt-4">
@@ -49,20 +53,21 @@
             </form>
         </div> 
             @foreach ($comments as $comment)
-                <div style='border:solid 1px; margin-bottom: 10px;'>
+                <div class="mt-10" style='border:solid 1px; margin-bottom: 10px;'>
                     <p>{{$comment->user->name}}</p>
                     <p>{{$comment->body}}</p>
                 </div>
-                <span>
-                    <img src="{{asset('img/nicebutton.png')}}" width="30px">
+                <div class="text-right">
+                    {{--<img src="{{asset('img/nicebutton.png')}}" width="30px">--}}
                      
                     <!-- もし$niceがあれば＝ユーザーが「いいね」をしていたら -->
                     @if($comment->isliked_comment())
                     <!-- 「いいね」取消用ボタンを表示 -->
-                    	<a href="{{ route('comment.unnice', $comment->id) }}" class="btn btn-success btn-sm">
+                    	<a href="{{ route('comment.unnice', $comment->id) }}" class="btn btn-success btn-sm block">
                     		いいね
+                    		❤
                     		<!-- 「いいね」の数を表示 -->
-                    		<span class="badge">
+                    		<span class="text-red-500" class="badge">
                     			{{ $comment->comment_likes->count() }}
                     		</span>
                     	</a>
@@ -70,17 +75,19 @@
                     <!-- まだユーザーが「いいね」をしていなければ、「いいね」ボタンを表示 -->
                     	<a href="{{ route('comment.nice', $comment->id) }}" class="btn btn-secondary btn-sm">
                     		いいね
+                    		🤍
                     		<!-- 「いいね」の数を表示 -->
                     		<span class="badge">
                     			{{ $comment->comment_likes->count() }}
                     		</span>
                     	</a>
                     @endif
-                </span>
-                <form method="post" action="{{route('comment.destroy', $comment)}}">
+                </div>
+                <form class="text-right relative" method="post" action="{{route('comment.destroy', $comment)}}">
                 @csrf
                 @method('delete')
-                <button type="submit" class="btn btn-danger" onClick="return confirm('本当に削除しますか？');">削除</button>
+                    <button type="submit" class="block right-0 absolute" onClick="return confirm('本当に削除しますか？');">削除</button>
                 </form>
             @endforeach
+    </div>
 </x-app-layout>
